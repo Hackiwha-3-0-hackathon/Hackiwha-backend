0611504ad10461a4a629e21e2958711159dbbb5a
Naaylla <rahmoune.nayla@gmail.com>
2026-07-10 12:51:07 +0100

    fix: adding model import so Alembic can discover them

---
+++ app/db/base.py
@@ -0,0 +1,6 @@
+from app.db.base_class import Base 
+from app.models.user import User 
+from app.models.profile import Profile  
+from app.models.brand import Brand  
+from app.models.campaign import Campaign 
+from app.models.application import Application 
